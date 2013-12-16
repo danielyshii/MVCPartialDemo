fcc7c73298dae444c5f82bcd81c3fd376381acee
Daniel Yshii <andulink@gmail.com>
2013-12-15 23:23:17 -0500

    First Push

---
+++ DemoJavascriptPartial/Controllers/CommonController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoJavascriptPartial.Controllers
+{
+    public class CommonController : Controller
+    {
+        //
+        // GET: /Common/
+
+        public ActionResult ImageCarrousel()
+        {
+
+            return PartialView();
+        }
+
+    }
+}
---
+++ DemoJavascriptPartial/Scripts/Common/ImageManager.js
@@ -0,0 +1,48 @@
+﻿ImageManager = function (config) {
+
+    var self = this;
+
+    self.config = {
+        container: null,
+        clickHandler: self.DefaultClickHandler
+    }
+
+    self.Init = function (config)
+    {
+        self.config.container = config.container;
+        self.config.clickHandler = config.clickHandler;
+
+        self.loadPartial();
+    }
+
+    self.DefaultClickHandler = function () {
+
+        alert('On Default Click Handler');
+
+    }
+
+    self.ConfigureImageHandler = function () {
+
+        $('.carrousel-container .img-click').click(self.config.clickHandler);
+
+    }
+
+    self.loadPartial = function ()
+    {
+        $.ajax({
+            type: "GET",
+            url: '/Common/ImageCarrousel',
+            dataType: "html",
+            success: function (data) {
+                $(self.config.container).empty();
+                $(self.config.container).html(data);
+
+                self.ConfigureImageHandler();
+            }
+
+        });
+    }
+
+    self.Init(config);
+
+}
